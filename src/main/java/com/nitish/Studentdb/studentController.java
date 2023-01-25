f261c60caeb532081309c9e49b710b57bcf3bfad
nitish <diluboruah@gmail.com>
2023-01-25 16:43:51 +0530

    commit2nd

--- src/main/java/com/nitish/Studentdb/studentController.java
+++ src/main/java/com/nitish/Studentdb/studentController.java
@@ -10,14 +10,14 @@ public class studentController {
     Map<Integer,Student> db= new HashMap<>();
 
     @PostMapping ("/add_student") //POST endpoint
-    public String addStudent(@RequestBody Student student){
+    public String addStudent(@RequestBody Student student){ //requestBody instead of taking single argument taking student
         if(student==null) return "Invalid Input";
         db.put(student.getEnrollNo(),student);
         return "Successfully ADDED";
     }
 
     @GetMapping ("/get_student") //GET endpoint
-    public Student getStudent(@RequestParam("q") int enrollNo){
+    public Student getStudent(@RequestParam("q") int enrollNo){  //request param q=enrollNo
 
         return db.get(enrollNo);
     }
